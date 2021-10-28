a6763a5ec4981ded14785b49b1f1c4357f61690e
TrKienn <92080373+kien15042003@users.noreply.github.com>
2021-10-28 14:34:40 +0700

    Add files via upload

---
+++ BTVN 02.cpp
@@ -0,0 +1,29 @@
+// BTVN 02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, x;
+    cout << "giai pt ax + b =  0" << endl;
+    cout << "a = ";
+    cin >> a; '\n';
+    cout << "b = ";
+    cin >> b; '\n';
+    x = -b / a;
+    cout << "x = " << x << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
